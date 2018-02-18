3053e1b5788206f10847a7ad8d9578d1bc366cde
Kiril Raykov <kirilraykov@gmail.com>
2018-02-19 01:11:09 +0200

    Fixing conflicts

--- src/main/java/com/kiril/maven_git_test_field/mvn_artifact/App.java
+++ src/main/java/com/kiril/maven_git_test_field/mvn_artifact/App.java
@@ -7,8 +7,8 @@ package com.kiril.maven_git_test_field.mvn_artifact;
 public class App 
 {
     public static void main( String[] args ){
-    	//a changes on the master from the other developers
-        System.out.println( "Hello World! - for feature 3" );
+    	//a changes on the master from me
+        System.out.println( "Hello World! - for feature 1" );
         
         //changes from the feature 2
         System.out.println("feature 2");
