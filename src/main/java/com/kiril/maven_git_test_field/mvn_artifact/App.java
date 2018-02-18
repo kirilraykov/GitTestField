873f2f0f81fe3213dbb81b5cee48c55bb1a38f70
Kiril Raykov <kirilraykov@gmail.com>
2018-02-19 00:16:37 +0200

    Initial Commit

---
+++ src/main/java/com/kiril/maven_git_test_field/mvn_artifact/App.java
@@ -0,0 +1,13 @@
+package com.kiril.maven_git_test_field.mvn_artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
