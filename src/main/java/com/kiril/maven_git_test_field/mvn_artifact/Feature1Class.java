19850e2f2718fbc2d7fb122b9b33b739b99be0af
Kiril Raykov <kirilraykov@gmail.com>
2018-02-27 22:50:18 +0200

    Update Feature1Class.java

--- src/main/java/com/kiril/maven_git_test_field/mvn_artifact/Feature1Class.java
+++ src/main/java/com/kiril/maven_git_test_field/mvn_artifact/Feature1Class.java
@@ -2,7 +2,7 @@ package com.kiril.maven_git_test_field.mvn_artifact;
 
 public class Feature1Class {
 
-	//a new change on the master by the developers
+	//A remote change #2
 	public Feature1Class() {
 		// this is a test for a feature 1
 	}
